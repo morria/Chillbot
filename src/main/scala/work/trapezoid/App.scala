bc8a882a36f5dad33d37a32e59c92aea35bfdc24
Andrew S. Morrison <morria@users.noreply.github.com>
2021-07-10 10:29:03 -0400

    ...

--- src/main/scala/work/trapezoid/App.scala
+++ src/main/scala/work/trapezoid/App.scala
@@ -13,6 +13,12 @@ import com.slack.api.model.block.element.BlockElements.{asElements, button}
 import com.slack.api.bolt.{App => BoltApp}
 import com.slack.api.bolt.jetty.SlackAppServer
 
+/**
+ * App expects env variables SLACK_TOKEN, SLACK_BOT_TOKEN
+ * and SLACK_SIGNING_SECRET.
+ *
+ * SLACK_TOKEN and SLACK_BOT_TOKEN will probably be the same
+ */
 object App {
 
    def main(args : Array[String]) {
@@ -27,7 +33,7 @@ object App {
          asBlocks(
             divider(),
             section(section => section.text(markdownText(":zap: *Now online*"))),
-            actions((actions:ActionsBlockBuilder) => actions.elements(asElements(
+            actions((actions:ActionsBlockBuilder) => actions.blockId("now-online").elements(asElements(
                   button(b => b.text(plainText((pt:PlainTextObjectBuilder) => pt.emoji(true).text("Alpha"))).value("v1")),
                   button(b => b.text(plainText((pt:PlainTextObjectBuilder) => pt.emoji(true).text("Beta"))).value("v2"))
                ))
@@ -40,11 +46,19 @@ object App {
       val app:BoltApp = new BoltApp()
 
       app.command("/chill", (req, ctx) => {
-        // println("Got command /hello" + req)
-        // ctx.logger.info("respond result - {}", req)
-        // val res = ctx.respond(it.text(":wave: Hello!"))
         ctx.ack(":wave: Hello!")
       })
+
+      // when a user clicks a button in the actions block
+      app.blockAction("now-online", (req, ctx) => {
+        val value:String  = req.getPayload().getActions().get(0).getValue(); // "button's value"
+        if (req.getPayload().getResponseUrl() != null) {
+          // Post a message to the same channel if it's a block in a message
+          ctx.respond("You've sent " + value + " by clicking the button!")
+        }
+        ctx.respond("ok")
+        ctx.ack()
+      });
 
       val server:SlackAppServer = new SlackAppServer(app, 3000)
       server.start() // http://localhost:3000/slack/events
@@ -52,5 +66,4 @@ object App {
       // val rtm = slack.rtm(token)
       // rtm.connect()
    }
-
 }
