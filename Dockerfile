957f48c1854626e18a77a8afea03618d14e2fe91
Andrew S. Morrison <morria@users.noreply.github.com>
2021-07-29 15:31:35 -0400

    ...

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ MAINTAINER Andrew S. Morrison "asm@trapezoid.work"
 
 COPY target /usr/src/myapp
 WORKDIR /usr/src/myapp
-CMD ["java", "-jar", "Chillbot-1.0-SNAPSHOT-jar-with-dependencies.jar"]
+CMD ["java", "-jar", "Chillbot-1.0-SNAPSHOT-jar-with-dependencies.jar", "-DslackLogLevel=debug", "-Dorg.slf4j.simpleLogger.defaultLogLevel=debug", "-Dorg.eclipse.jetty.util.log.class.LEVEL=DEBUG"]
--- src/main/scala/work/trapezoid/App.scala
+++ src/main/scala/work/trapezoid/App.scala
@@ -13,6 +13,8 @@ import com.slack.api.model.block.element.BlockElements.{asElements, button}
 import com.slack.api.bolt.{App => BoltApp}
 import com.slack.api.bolt.jetty.SlackAppServer
 
+import org.slf4j.{Logger, LoggerFactory}
+
 /**
  * App expects env variables SLACK_TOKEN, SLACK_BOT_TOKEN
  * and SLACK_SIGNING_SECRET.
@@ -21,8 +23,16 @@ import com.slack.api.bolt.jetty.SlackAppServer
  */
 object App {
 
+  private val logger: Logger = LoggerFactory.getLogger(classOf[App])
+
    def main(args : Array[String]) {
       val config:SlackConfig = new SlackConfig();
+
+      System.setProperty("org.eclipse.jetty.util.log.class", "org.eclipse.jetty.util.log.JavaUtilLog");
+      System.setProperty("org.eclipse.jetty.util.log.class.LEVEL", "DEBUG");
+      System.setProperty("org.slf4j.simpleLogger.log.com.slack.api", "debug");
+
+
       config.setPrettyResponseLoggingEnabled(true);
 
       val slack = Slack.getInstance(config)
@@ -34,30 +44,47 @@ object App {
             divider(),
             section(section => section.text(markdownText(":zap: *Now online*"))),
             actions((actions:ActionsBlockBuilder) => actions.blockId("now-online").elements(asElements(
-                  button(b=> b.actionId("chillbot-action").text(plainText((pt:PlainTextObjectBuilder) => pt.emoji(true).text("Alpha"))).value("v1")),
-                  button(b => b.actionId("chillbot-action").text(plainText((pt:PlainTextObjectBuilder) => pt.emoji(true).text("Beta"))).value("v2"))
+                  button(b=> b.actionId("chillbot-action-alpha").text(plainText((pt:PlainTextObjectBuilder) => pt.emoji(true).text("Alpha"))).value("v1")),
+                  button(b => b.actionId("chillbot-action-beta").text(plainText((pt:PlainTextObjectBuilder) => pt.emoji(true).text("Beta"))).value("v2"))
                ))
             )
          )
       ).build)
+      println("Got response " + response)
 
       // App expects env variables (SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET)
       val app:BoltApp = new BoltApp()
 
       app.command("/chill", (req, ctx) => {
+        logger.info("Got /chill");
+        logger.debug("Got /chill");
         ctx.ack(":wave: Hello!")
       })
 
       // when a user clicks a button in the actions block
-      app.blockAction("chillbot-action", (req, ctx) => {
+      app.blockAction("chillbot-action-alpha", (req, ctx) => {
+        logger.info("Got chillbot-action-alpha");
         val value:String  = req.getPayload().getActions().get(0).getValue(); // "button's value"
         if (req.getPayload().getResponseUrl() != null) {
           // Post a message to the same channel if it's a block in a message
-          ctx.respond("You've sent " + value + " by clicking the button!")
+          ctx.respond("Alpha: " + value)
         }
-        ctx.respond("ok")
+        ctx.respond("Alpha => " + value)
         ctx.ack()
       });
+
+      // when a user clicks a button in the actions block
+      app.blockAction("chillbot-action-beta", (req, ctx) => {
+        logger.info("Got chillbot-action-beta");
+        val value:String  = req.getPayload().getActions().get(0).getValue(); // "button's value"
+        if (req.getPayload().getResponseUrl() != null) {
+          // Post a message to the same channel if it's a block in a message
+          ctx.respond("Beta: " + value)
+        }
+        ctx.respond("Beta => " + value)
+        ctx.ack()
+      });
+
 
       val server:SlackAppServer = new SlackAppServer(app, 3000)
       server.start() // http://localhost:3000/slack/events
